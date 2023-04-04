7ed63e96f0d98904af7f41b717c2a849fe335a1e
yelim1215 <yerim1215jg@naver.com>
2023-04-04 12:45:26 +0900

    17. @Repository 에서 쿼리 추가하기

--- src/main/java/com/example/demo/service/TodoService.java
+++ src/main/java/com/example/demo/service/TodoService.java
@@ -14,11 +14,11 @@ public class TodoService {
 	
 	public String testService() {
 		// Todo Entity 생성
-		TodoEntity entity = TodoEntity.builder().title("My first todo item").build();
+		TodoEntity entity = TodoEntity.builder().userId("user01").title("My first todo item").build();
 		// Todo Entity 저장
 		repository.save(entity);
 		// Todo Entity 검색
-		TodoEntity savedEntity = repository.findById(entity.getId()).get();
-		return savedEntity.getTitle();
+		TodoEntity savedEntity = repository.findByUserId(entity.getUserId()).get(0);
+		return savedEntity.getUserId();
 	}
 }
