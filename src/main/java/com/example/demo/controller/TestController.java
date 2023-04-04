a31bb49047cfc24300241b5af74974910ae5aa9a
yelim1215 <yerim1215jg@naver.com>
2023-04-04 09:39:15 +0900

    15. @Service 사용하기

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -3,52 +3,30 @@ package com.example.demo.controller;
 import java.util.ArrayList; // 만약 자동으로 import 작성 안되면 직접 작성한다.
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.dto.ResponseDTO;
-import com.example.demo.dto.TestRequestBodyDTO;
+import com.example.demo.service.TodoService;
 
 import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
-@RequestMapping("test")
+@RequestMapping("todo")
 public class TestController {
 	
-	@GetMapping
-	public String testController() {
-		return "Hello World!";
-	}
-	@GetMapping("/testRequestBody")
-	public String testControllerRequestBody(@RequestBody TestRequestBodyDTO testRequestBodyDTO) {
-		return "Hello World! ID " + testRequestBodyDTO.getId() + "Message : " + testRequestBodyDTO.getMessage();
-	}
-	@GetMapping("/testResponseBody")
-	public ResponseDTO<String>testControllerResponseBody() {
-		List<String> list = new ArrayList<String>();
-		list.add("Hello World! I'm ResponseDTO");
-		list.add("See you!");
-		ResponseDTO<String> response = ResponseDTO.<String>builder().data(list).build();
-		return response;
-	}
+	@Autowired
+	private TodoService service;
 	
-	@GetMapping("/testResponseEntityOK")
-	public ResponseEntity<?>testControllerResponseEntityOK() {
-		List<String> list = new ArrayList<String>();
-		list.add("Hello World! I'm ResponseEntity. And you get 200!");
-		list.add("See you!");
+	@GetMapping("/test")
+	public ResponseEntity<?>testTodo() {
+		String str = service.testService();
+		List<String> list = new ArrayList<>();
+		list.add(str);
 		ResponseDTO<String> response = ResponseDTO.<String>builder().data(list).build();
 		return ResponseEntity.ok().body(response);
-	}
-	
-	@GetMapping("/testResponseEntityBad")
-	public ResponseEntity<?>testControllerResponseEntityBad() {
-		List<String> list = new ArrayList<String>();
-		list.add("Hello World! I'm ResponseEntity. And you get 400!");
-		list.add("See you!");
-		ResponseDTO<String> response = ResponseDTO.<String>builder().data(list).build();
-		return ResponseEntity.badRequest().body(response);
 	}
 }
